eade3da1f9eab2da7fdb7e9d56514e6e566631c4
Gabriel Sousa <gabriel.gs@sidi.org.br>
2023-06-27 17:34:35 -0300

    translating meals practice from fixed values to database values

--- meal-practice/src/components/MealsList/MealsList.js
+++ meal-practice/src/components/MealsList/MealsList.js
@@ -1,44 +1,45 @@
+import { useEffect, useState } from "react"
 import MealItem from "../MealItem/MealItem"
 import Card from "../UI/Card/Card"
 import classes from './MealsList.module.css'
 
 const MealsList = props => {
-    const DUMMY_MEALS = [
-        {
-          id: 'm1',
-          name: 'Sushi',
-          description: 'Finest fish and veggies',
-          price: 22.99,
-        },
-        {
-          id: 'm2',
-          name: 'Schnitzel',
-          description: 'A german specialty!',
-          price: 16.5,
-        },
-        {
-          id: 'm3',
-          name: 'Barbecue Burger',
-          description: 'American, raw, meaty',
-          price: 12.99,
-        },
-        {
-          id: 'm4',
-          name: 'Green Bowl',
-          description: 'Healthy...and green...',
-          price: 18.99,
-        },
-      ];
+  const [meals, setMeals] = useState([])
 
-    const mealsList= DUMMY_MEALS.map(meal=> <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)      
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const mealsToAdd = []
+      const response = await fetch("https://reacthttp-65347-default-rtdb.firebaseio.com/meals.json")
 
-    return (
-        <Card className={`${classes.meals} ${classes.card}`}>
-            <ul>
-                {mealsList}
-            </ul>
-        </Card>
-    )
+      if (!response.ok) {
+        throw new Error('Error, please fix.')
+      }
+
+      const unformatedMeals = await response.json()
+
+      for (const key in unformatedMeals) {
+        mealsToAdd.push({
+          id: key,
+          name: unformatedMeals[key].name,
+          description: unformatedMeals[key].description,
+          price: unformatedMeals[key].price
+        })
+      }
+      setMeals(mealsToAdd)
+    }
+
+    fetchMeals()
+  }, [])
+
+  const mealsList = meals.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
+
+  return (
+    <Card className={`${classes.meals} ${classes.card}`}>
+      <ul>
+        {mealsList}
+      </ul>
+    </Card>
+  )
 }
 
 export default MealsList
